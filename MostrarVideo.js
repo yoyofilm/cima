8647cc8d40cebdf115c4f3ae1f7298becd33bdf9
yoyofilm <yahiaboda@yahoo.com>
2017-12-21 14:17:37 +0200

    Update MostrarVideo.js

--- MostrarVideo.js
+++ MostrarVideo.js
@@ -1,14 +1,14 @@
-function MostrarVideo(idok)
+function MostrarVideo(idopenload)
 {
 var contenedor = document.getElementById('divInnerVideo');
-if(idok == "")
+if(idopenload == "")
 {
 contenedor.innerHTML = "";
 }
 else
 {
 var dimensiones = "width='600' height='630' frameborder='0'";
-var url = "http://www.ok.ru/videoembed/" + idok;
+var url = "http://openload.co/embed/"+ idopenload;
 contenedor.innerHTML = "<iframe " + dimensiones + " src='"+ url +"'></iframe>";
 }
 }
